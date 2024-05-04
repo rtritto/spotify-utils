2d6e81a2699a044d7310c247f42c3ef643672bb5
rtritto <ariele.1994@gmail.com>
2024-05-04 18:36:22 +0200

    Update deps and start

--- start.ts
+++ start.ts
@@ -7,23 +7,36 @@ import {
   getUrisFromFiles
 } from './src/index.ts'
 
-// const playlist = await getPlaylist()
-// console.log('getPlaylist: OK');
+//#region getPlaylist
+const playlist = await getPlaylist({
+  playlist_id:
+    'XXXXXXXXXXXXXXXXXXXXXX'  // All
+})
+console.log('getPlaylist: OK')
+//#endregion
 
+//#region createPlaylist
 // const { id } = await createPlaylist({ name: 'Phonk 2' })
 // console.log('createPlaylist: OK');
+//#endregion
 
+//#region addItemsToPlaylist
 // await addItemsToPlaylist({ playlist_id: id, playlistFilepath: 'playlist-<NAME>-20240330T002732.json' })
 // console.log('addItemsToPlaylist: OK');
+//#endregion
 
-const uris = getUrisFromFiles('playlist-All-20240330T121626.json')
-console.log('uris: ', uris);
+//#region getUrisFromFiles
+// const uris = getUrisFromFiles('playlist-All-20240330T121626.json')
+// console.log('uris: ', uris);
 // Copy to Windows Clipboard
-const proc = spawn('clip')
-proc.stdin.write(JSON.stringify(uris))
-proc.stdin.end()
+// const proc = spawn('clip')
+// proc.stdin.write(JSON.stringify(uris))
+// proc.stdin.end()
+//#endregion
 
+//#region addItemsToPlaylist
 // await addItemsToPlaylist({ playlist_id: '', uris })
 // console.log('addItemsToPlaylist: OK');
 // await addItemsToPlaylist({ playlist_id: '', uris: ['spotify:track:5MNfAmHvsqOliIxtwiFzsz'] })
 // console.log('addItemsToPlaylist: OK');
+//#endregion
